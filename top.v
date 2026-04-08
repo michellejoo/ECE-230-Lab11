353b756ab28c801c528f9adb54b83b815065efec
Michelle Joo <michellejoo@u.boisestate.edu>
2026-04-08 14:10:18 -0600

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,26 @@
+module top(
+    input btnC,
+    input btnU,
+    output [6:0] led
+);
 
+    wire [2:0] rc_state;
+    wire [2:0] mc_state;
+    wire mc_out;
+
+    ripple_counter_3bit rc0(
+        .clk(btnC),
+        .rst(btnU),
+        .q(rc_state)
+    );
+
+    modulo_divider_6 mc0(
+        .clk(btnC),
+        .rst(btnU),
+        .state(mc_state),
+        .out(mc_out)
+    );
+
+    assign led = {mc_out, mc_state, rc_state};
+
+endmodule
